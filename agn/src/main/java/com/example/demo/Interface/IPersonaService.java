079cddfaa5943102a4e92d332bcffd41242f5509
Nievas2 <angelgabrielnievas@gmail.com>
2023-01-04 12:54:21 -0300

    avances

---
+++ agn/src/main/java/com/example/demo/Interface/IPersonaService.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.example.demo.Interface;
+
+import com.example.demo.Entity.Persona;
+import java.util.List;
+
+public interface IPersonaService {
+    public List<Persona> getPersona();
+    
+    public void savePersona(Persona persona);
+    
+    public void deletePersona(Long Id);
+    
+    public Persona findPersona(Long Id);
+    
+    
+    
+}
